851ed598b20d865c29969f68cb40c33d01d38a53
Alfi Noushad <Alfinoushad@gmail.com>
2025-06-05 19:58:51 +0530

    added profile

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ window.addEventListener('scroll', ()=>{
     }
 });
 
+const profileIcon = document.querySelector(".profile-icon");
+profileIcon.addEventListener("click", () => {
+  window.location.href = "profile.html";
+});
+
+const movieCard = document.querySelectorAll(".content-card");
+movieCard.forEach((card) => {
+  card.addEventListener("mouseEnter", () => {
+    card.style.transform = "scale(1.05)";
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "scale(1)";
+  });
+});
